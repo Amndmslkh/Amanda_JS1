f9954d023a8d204ef40ddb38b9f037c1392508f0
Amndmslkh <amndmslkh@gmail.com>
2023-10-08 13:17:20 +0700

    projrct java script

---
+++ script.js
@@ -0,0 +1,73 @@
+// If- Else
+//var nilai = 75;
+//if (nilai >= 70) {
+//  console.log("Anda lulus.");
+//} else {
+//  console.log("Anda gagal.");
+//}
+
+
+
+// nested If
+//var usia = 18;
+//var izin = true;
+//if (usia >= 18) {
+//  if (izin) {
+//    console.log("Anda bisa masuk ke klub.");
+//  } else {
+//    console.log("Anda tidak memiliki izin.");
+//  }
+//} else {
+//  console.log("Anda belum cukup usia.");
+//}
+
+
+// Switch Case
+//var hari = "Senin";
+//switch (hari) {
+// case "Senin":
+//    console.log("Hari ini adalah hari kerja.");
+//    break;
+//  case "Sabtu":
+//  case "Minggu":
+//    console.log("Hari ini adalah hari libur.");
+//    break;
+//  default:
+//    console.log("Hari ini tidak dikenal.");
+//}
+
+
+
+//For Statement
+//for (var i = 1; i <= 5; i++) {
+//    console.log("Hari ke-" + i);
+// }
+
+
+
+// While
+//var angka = 1;
+//while (angka <= 5) {
+//  console.log("Angka: " + angka);
+//  angka++;
+//}
+
+
+
+// do while
+//var counter = 1;
+//do {
+//  console.log("Perulangan ke-" + counter);
+//  counter++;
+//} while (counter <= 5);
+
+
+
+// function
+//function tambah(a, b) {
+//    return a + b;
+//  }
+//  
+//  var hasil = tambah(5, 3);
+//  console.log("Hasil penambahan: " + hasil);
+  
